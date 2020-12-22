1a2884202ffb71240cd84acb7a2045c5b7205222
Noyan <sngermiyanoglu@hotmail.com>
2020-12-22 15:07:26 +0300

    Task 3: Added description comments of all functions in Auhtneticated User Action

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,5 +1,9 @@
 import { SET_AUTHED_USER,LOGOUT_USER } from "../constants/Constants";
 
+/**
+ * @description Sets the "Authed User" ID within authedUser store
+ * @param {string} id
+ */
 export function setAuthedUser (id) {
     return{
       type: SET_AUTHED_USER,
@@ -7,6 +11,9 @@ export function setAuthedUser (id) {
     }
 }
   
+/**
+ * @description Remove the "Authed User" from authedUser store
+ */
 export function logoutUser(){
     return{
       type: LOGOUT_USER,

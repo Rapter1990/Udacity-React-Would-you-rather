7f18c398802a9abf49c912d326c95abfecb383b4
Noyan <sngermiyanoglu@hotmail.com>
2020-12-23 11:51:53 +0300

    Task 4 : Installed react-redux-loading and Completed Question Action

---
+++ src/actions/questions.js
@@ -0,0 +1,115 @@
+import { saveQuestion, saveQuestionAnswer } from "../utils/_DATA";
+import { RECEIVE_QUESTIONS ,ANSWER_QUESTION,CREATE_QUESTION } from "../constants/Constants";
+import { showLoading, hideLoading } from 'react-redux-loading';
+
+
+/**
+ * @description Fills all questions into store by getting a questions object fetched from API
+ * @param {object} questions
+ */
+export function getQuestions(questions) {
+    return {
+      type: RECEIVE_QUESTIONS,
+      questions,
+    };
+  }
+
+/**
+ * @description Creates question putting into the store
+ * @param {object} question - Question object returning as a response from API
+ */
+function createQuestion(question) {
+    return {
+        type: CREATE_QUESTION,
+        question
+    }
+}  
+
+/**
+ * @description Answers question as a modified answer putting into the store
+ * @param {string} authedUser - authedUser variable from question object
+ * @param {string} questionid - questionid variable from question object
+ * @param {string} answer - answer variable from question object
+ */
+function answerQuestion ({ authedUser, questionid, answer }) {
+    return {
+        type: ANSWER_QUESTION,
+        authedUser,
+        questionid,
+        answer
+    }
+}
+
+/**
+ * @description Creates a new question on backend side by communicating with API and put into store
+ * @param {string} optionOneText - Option one text of question
+ * @param {string} optionTwoText - Option two text of question
+ */
+export function handleCreateQuestion(optionOneText, optionTwoText) {
+    return (dispatch, getState) => {
+
+        /** TODO : Get authedUser from combineReducer via getState() */
+        const { authedUser } = getState()
+
+        dispatch(showLoading())
+
+        const questionInfo = {
+            author: authedUser,
+            optionOneText,
+            optionTwoText
+        }
+
+        return saveQuestion(questionInfo)
+            .then((question) => {
+                    dispatch(createQuestion(question))
+                }
+            )
+            .catch((error) => {
+                    console.warn("ERROR --> handleCreateQuestion : ", error);
+                    alert("ERROR : Adding your question to the database")
+                }
+            )
+            .finally(() => {
+                    dispatch(hideLoading());
+                }
+            );
+    }
+}
+
+/**
+ * @description Answer a question on backend side by communicating with API and put into store 
+ * @param {string} authedUser - Authed user ID
+ * @param {string} qid  - Question ID
+ * @param {string} answer - Answer of question ( optionOne , optionTwo)
+ */
+export function handleAnswerQuestion(authedUser, qid, answer) {
+
+    return (dispatch) => {
+
+        dispatch(showLoading());
+
+        return saveQuestionAnswer({
+          authedUser,
+          qid,
+          answer,
+        })
+          .then(() => {
+            dispatch(
+              answerQuestion({
+                authedUser,
+                qid,
+                answer,
+              })
+            );
+          })
+          .catch((error) => {
+                console.warn("ERROR --> handleAnswerQuestion : ", error);
+                alert("ERROR : Adding your question to the database")
+            }
+          )
+          .finally(() => {
+                dispatch(hideLoading());
+            }
+          );
+    };
+}
--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,4 +1,12 @@
-// authed user
+/**
+ * @description Constants for Authed User Actions
+ */
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
 export const LOGOUT_USER = 'LOGOUT_USER'
 
+/**
+ * @description Constants for Question Actions
+ */ 
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const ANSWER_QUESTION = "ANSWER_QUESTION";
+export const CREATE_QUESTION = "CREATE_QUESTION";

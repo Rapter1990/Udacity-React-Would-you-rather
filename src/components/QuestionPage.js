222cb3e97927e8ab9708ecb634a5cd19b66a207a
Noyan <sngermiyanoglu@hotmail.com>
2020-12-30 01:17:02 +0300

    Task 42 : Devised Card View in QuestionPage Component

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,31 +1,70 @@
 import React, { Component } from 'react'
-import { formatNewQuestion } from '../utils/helper';
 import { connect } from 'react-redux';
+import { Container, Row, Col, Card, Image } from 'react-bootstrap';
 
  class QuestionPage extends Component {
 
+    styles = {
+        cardImage: {
+          width : 250,
+          height : 250,
+          alignSelf: 'center',
+          marginTop : 30
+        },
+        cardText: {
+            alignSelf: 'center'
+        }
+    }
 
     render() {
 
-        const { question, authedUser } = this.props;
+        const { question, authedUser, user } = this.props;
 
-        console.log("question : " , question , " | authedUser : " , authedUser)
+        const { optionOne, optionTwo } = question;
+
+        const { name , avatarURL} = user;
+
+        console.log("question : " , question , 
+                    " | authedUser : " , authedUser, 
+                    " | user : " , user)
+
 
         return (
-            <div>
-                QuestionPage 
-            </div>
+            <Container>
+                <Row className="mt-4">
+                    <Col
+                        md={{ span: 10, offset: 1 }}
+                        lg={{ span: 10, offset: 1 }}
+                    >
+                    <Card>
+                        <Card.Header>{name} asks </Card.Header>
+                        <Image
+                            className="mb-4"
+                            src={avatarURL}
+                            alt={`image of ${name}`}
+                            roundedCircle
+                            style={this.styles.cardImage}                     
+                        />
+                        <Card.Text as="h3" className="mb-3" style={this.styles.cardText}>
+                            Would you rather
+                        </Card.Text>
+                    </Card>
+                    </Col>
+                </Row>
+            </Container> 
         )
     }
 }
 
 function mapStateToProps({ authedUser, users, questions }, { id }) {
     const question = questions[id];
-  
+
+    const { author } = question;
+
     return {
       authedUser,
-      question: question
-        ? formatNewQuestion(question, users[question.author]) : null,
+      question,
+      user: users[author]
     };
   }
 

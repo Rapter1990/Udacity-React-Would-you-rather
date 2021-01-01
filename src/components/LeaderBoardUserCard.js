89df5a2cd9c55bf39007f927635a9175511eb716
Noyan <sngermiyanoglu@hotmail.com>
2021-01-01 14:46:25 +0300

    Task 51 : Continued the layout design of LeaderBoardUserCard Component

--- src/components/LeaderBoardUserCard.js
+++ src/components/LeaderBoardUserCard.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react'
+import { Row, Card , Col, Image} from 'react-bootstrap';
 
-export default class LeaderBoardUserCard extends Component {
+class LeaderBoardUserCard extends Component {
+
+
+    styles = {
+        cardImage: {
+          width : 200,
+          height : 200,
+          marginTop : 30
+        },
+        card: {
+          width: '18rem',
+          marginBottom : 10  
+        }
+    }
+
     render() {
 
         const { user } = this.props;
 
-        console.log("user : " , user);
+        const { name, avatarURL , answers , questions } = user;
+
+        const questionsSize = questions.length;
+        const answersSize = Object.keys(answers).length;
+
+        console.log("questionsSize : " , questionsSize , " | answersSize : ", answersSize)
+
+        const totalResult = questionsSize + answersSize;
+
+        console.log("totalResult : " , totalResult)
 
         return (
-            <div>
-                
-            </div>
+
+            <Col
+                xs={12}
+                md={{ span: 10, offset: 1 }}
+                lg={{ span: 10, offset: 1 }}
+                className="mb-4"
+            >
+                <Card className="text-center">
+                <Row as={Card.Body}>
+                    <Col xs={12} md={4}>
+                        <Image
+                            className="mb-4"
+                            src={avatarURL}
+                            alt={`image of ${name}`}
+                            roundedCircle
+                            style={this.styles.cardImage}                     
+                        />
+                    </Col>
+
+                    <Col xs={12} md={8} className="text-left">
+
+
+                    </Col>
+                </Row>
+
+                </Card>
+            </Col>
         )
     }
 }
+
+export default LeaderBoardUserCard;

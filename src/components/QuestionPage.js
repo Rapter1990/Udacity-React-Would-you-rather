c64cbee934124ebb2622d1c5b6c1e41d44406933
Noyan <sngermiyanoglu@hotmail.com>
2021-01-05 14:14:40 +0300

    Task 61: Added button diasble feature in QuestionPage Component

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -107,6 +107,7 @@ import { withRouter } from 'react-router-dom';
                                     onClick={this.handleAnsweredQuestion}
                                     className="mb-3"
                                     variant="primary"
+                                    disabled={this.state.selectedOption === 'optionOne' || this.state.selectedOption === 'optionTwo'}
                                 >
                                     Submit
                                 </Button>
